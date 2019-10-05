acfae179af7ddc4637cf8295e41fb10b9e14de01
Italo Josep <italojosep@gmail.com>
2019-10-05 11:57:21 -0300

    Donut chart layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { RouterModule } from '@angular/router';
-
 
 // Componentes
 import { AppComponent } from './app/app.component';
-import { HomeComponent } from './ui/home/home.component';
-import { HeaderComponent } from './ui/header/header.component';
+import { DonutChartComponent } from './ui/donut-chart/donut-chart.component';
 import { FabMoreComponent } from './ui/fab-more/fab-more.component';
+import { HeaderComponent } from './ui/header/header.component';
+import { HomeComponent } from './ui/home/home.component';
 import { ItemListHomeComponent } from './ui/item-list-home/item-list-home.component';
 
 // Routes
@@ -17,10 +16,12 @@ import { appRoutes } from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    HeaderComponent,
+    DonutChartComponent,
     FabMoreComponent,
+    HeaderComponent,
+    HomeComponent,
     ItemListHomeComponent
+    
   ],
   imports: [
     BrowserModule,
